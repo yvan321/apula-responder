2deff91c50f15954386cbaa2b42ca392370166c6
yvan321 <neilyvancaliwan@gmail.com>
2025-11-15 17:13:00 +0800

    Add icon

--- apula_responder/lib/main.dart
+++ apula_responder/lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     const primaryRed = Color(0xFFA30000);
-
+  
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Apula",
